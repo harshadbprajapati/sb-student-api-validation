792a4d02cd97b175ee84c167ca64eb17abd957f5
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2024-05-28 21:19:07 +0530

    feat(StudentDto): add all arguments constructor

--- src/main/java/com/example/studentapi/student/StudentDto.java
+++ src/main/java/com/example/studentapi/student/StudentDto.java
@@ -2,10 +2,12 @@ package com.example.studentapi.student;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class StudentDto {
     private Long id;
