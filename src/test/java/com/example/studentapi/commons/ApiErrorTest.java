36b3a29b0e19e2ef921c7728337935f0780c0c90
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2024-05-28 21:25:42 +0530

    test(ApiError): add tests for constructor, getters, setters

--- src/test/java/com/example/studentapi/commons/ApiErrorTest.java
+++ src/test/java/com/example/studentapi/commons/ApiErrorTest.java
@@ -1,4 +1,39 @@
+package com.example.studentapi.commons;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ApiErrorTest {
-  
+    @Test
+    void testApiError_ConstructorAndGetters() {
+        // Arrange
+        String field = "testField";
+        String message = "testMessage";
+
+        // Act
+        ApiError apiError = new ApiError(field, message);
+
+        // Assert
+        assertNotNull(apiError);
+        assertEquals(field, apiError.getField());
+        assertEquals(message, apiError.getMessage());
+    }
+
+    @Test
+    void testApiError_SetterAndGetters() {
+        // Arrange
+        ApiError apiError = new ApiError();
+        String field = "testField";
+        String message = "testMessage";
+
+        // Act
+        apiError.setField(field);
+        apiError.setMessage(message);
+
+        // Assert
+        assertEquals(field, apiError.getField());
+        assertEquals(message, apiError.getMessage());
+    }
+
 }
