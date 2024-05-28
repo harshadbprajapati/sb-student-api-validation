57662fa58b771e1cca2c1b45917810082e4e0539
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2024-05-28 21:37:24 +0530

    test(ModelMapperConfig): add tests for modelMapper and mappings

--- src/test/java/com/example/studentapi/commons/ModelMapperConfigTest.java
+++ src/test/java/com/example/studentapi/commons/ModelMapperConfigTest.java
@@ -1,4 +1,41 @@
+package com.example.studentapi.commons;
+
+import com.example.studentapi.student.Student;
+import com.example.studentapi.student.StudentDto;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ModelMapperConfigTest {
-  
+    @Test
+    void testModelMapperBeanCreation() {
+        // Arrange
+        ModelMapperConfig modelMapperConfig = new ModelMapperConfig();
+
+        // Act
+        ModelMapper modelMapper = modelMapperConfig.modelMapper();
+
+        // Assert
+        assertNotNull(modelMapper);
+    }
+
+    @Test
+    void testModelMapperMappings() {
+        // Arrange
+        ModelMapperConfig modelMapperConfig = new ModelMapperConfig();
+        ModelMapper modelMapper = modelMapperConfig.modelMapper();
+
+        // Act
+        TypeMap<StudentDto, Student> dtoToEntityMapping = modelMapper.getTypeMap(StudentDto.class, Student.class);
+        TypeMap<Student, StudentDto> entityToDtoMapping = modelMapper.getTypeMap(Student.class, StudentDto.class);
+
+        // Assert
+        assertNotNull(dtoToEntityMapping,
+                "Mapping from StudentDto to Student is not configured");
+        assertNotNull(entityToDtoMapping,
+                "Mapping from Student to StudentDto is not configured");
+    }
+
 }
