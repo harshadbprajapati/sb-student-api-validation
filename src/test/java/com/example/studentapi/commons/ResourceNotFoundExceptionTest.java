de0b79cfcd8f3f75e6a3edd2e253b8491b289f44
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2024-05-28 21:32:38 +0530

    test(ResourceNotFoundException): add test for generating exception message

--- src/test/java/com/example/studentapi/commons/ResourceNotFoundExceptionTest.java
+++ src/test/java/com/example/studentapi/commons/ResourceNotFoundExceptionTest.java
@@ -1,4 +1,22 @@
+package com.example.studentapi.commons;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ResourceNotFoundExceptionTest {
-  
+
+    @Test
+    void testResourceNotFoundException_Exception_ShouldReturnExceptionMessage() {
+        // Arrange
+        String errorMessage = "Resource not found";
+
+        // Act
+        ResourceNotFoundException exception = new ResourceNotFoundException(errorMessage);
+
+        // Assert
+        assertNotNull(exception);
+        assertEquals(errorMessage, exception.getMessage());
+    }
+
 }
