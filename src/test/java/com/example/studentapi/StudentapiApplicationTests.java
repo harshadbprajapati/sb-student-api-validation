ccb767e50847750cce2d4205c691287a5a525f23
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2024-05-31 18:04:25 +0530

    test(StudentapiApplication): add test for loading of controller

--- src/test/java/com/example/studentapi/StudentapiApplicationTests.java
+++ src/test/java/com/example/studentapi/StudentapiApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.studentapi;
 
+import com.example.studentapi.student.StudentController;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StudentapiApplicationTests {
+	@Autowired
+	private StudentController studentController;
 
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(studentController).isNotNull();
 	}
 
 }
